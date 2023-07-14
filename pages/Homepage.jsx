ac652a14d0c9ec787208df34c10c85a9eb28d0dc
korbanbanjir <gelora.dam2003@mail.ugm.ac.id>
2023-07-14 16:45:28 +0700

    add: home page

---
+++ pages/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Homepage = () => {
+
+    return (    
+        <div>
+            <h1>Homepage</h1>
+            <div className='bg-[navbar-pattern] w-full h-20'>
+                <p>test</p>
+
+            </div>
+        </div>
+        
+    );
+}
+
+export default Homepage;
