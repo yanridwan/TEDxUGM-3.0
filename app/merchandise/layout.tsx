138ea8584735de18662527922e7800c4ceeaa9e1
khairulihsannudin <khairulihsannudin@mail.ugm.ac.id>
2023-07-21 11:17:28 +0700

    Layout merch edit

--- app/merchandise/layout.tsx
+++ app/merchandise/layout.tsx
@@ -5,10 +5,12 @@ export default function MerchLayout({
     children: React.ReactNode
 }) {
     return (
-        <main className="overflow-x-hidden w-screen">
-            <h2 className="absolute -top-20 -right-[25vw] font-outline-1 text-white text-[10rem]">Merchandise</h2>
-            <h2 className="absolute top-0 -left-[35vw] font-outline-1 text-white text-[10rem]">Merchandise</h2>
-            <div className="relative z-20">{children}</div>
+        <main className="overflow-x-hidden">
+            <div className="max-md:hidden">
+            <h2 className="absolute -top-[calc(2vh+0.4em)] -right-[calc(10vw+2em)] font-outline-1 text-white text-[calc(10vw+3em)]">Merchandise</h2>
+            <h2 className="absolute top-0 -left-[calc(10vw+2em)] font-outline-1 text-white text-[calc(10vw+3em)]">Merchandise</h2>
+            </div>
+            <div className="absolute z-20">{children}</div>
         </main>
     )
 }
