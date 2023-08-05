bf779142aef6e292179cc8733a63b34bc4971d90
Khairul <khairulihsannudin@mail.ugm.ac.id>
2023-08-05 22:06:54 +0700

    minor fixing

--- app/merchandise/page.tsx
+++ app/merchandise/page.tsx
@@ -10,14 +10,14 @@ export default function Merchandise() {
     return (
         <main className="relative w-full h-full">
             <div className="absolute -z-20 w-full h-full max-md:hidden">
-                <h2 className="absolute -top-[calc(2vh+0.5em)] -right-[calc(10vw+2em)] layoutTitle">Merchandise</h2>
-                <h2 className="absolute top-0 -left-[calc(10vw+2em)] layoutTitle">Merchandise</h2>
+                <h2 className="absolute -top-[calc(2vh+0.3em)] -right-[calc(10vw+2em)] layoutTitle">Merchandise</h2>
+                <h2 className="absolute top-10 -left-[calc(10vw+2em)] layoutTitle">Merchandise</h2>
                 <h2 className="absolute bottom-0 -right-[calc(10vw+2em)] layoutTitle">Merchandise</h2>
                 <h2 className="absolute -bottom-[calc(2vh+0.5em)] -left-[calc(10vw+2em)] layoutTitle">Merchandise</h2>
             </div>
             <div>
             <section className="w-full text-center">
-                <h1 className="mt-10 titleRed">OUR MERCHANDISE</h1>
+                <h1 className="pt-20 titleRed">OUR MERCHANDISE</h1>
             </section>
             <section className="md:max-w-[80%] mx-auto gap-10 grid grid-flow-rows-dense grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 max-md:mx-20 mt-20">
                 {merchs.length != 0 ?
@@ -30,7 +30,7 @@ export default function Merchandise() {
                                 image: merch.image
                             }
                         }}>
-                            <div className="mb-10">
+                            <div className="">
                                 <div className="merchCard border-black border bg-white rounded-3xl aspect-square px-2 flex items-center justify-center mb-2">
                                     <img src={merch.image} alt={merch.name} />
                                 </div>
