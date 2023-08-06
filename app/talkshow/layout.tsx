7f3baeb43d1a1725ecc1595066311c82541cb5c0
Khairul <khairulihsannudin@mail.ugm.ac.id>
2023-08-06 21:34:56 +0700

    Talkshow Page

--- app/talkshow/layout.tsx
+++ app/talkshow/layout.tsx
@@ -1,15 +1,15 @@
-function Interview() {
+function TalkshowLayout({children}:{children:React.ReactNode}) {
     return (
-      <div class="relative bg-black min-h-screen ">
+      <div class="bg-white ">
       <div class="fixed box-content bg-white w-full h-16">
         <img src="interview/arrow.svg" className="w-5 h-5 absolute left-10 top-5"/>
-        <h1 className="absolute left-16 font-bold top-5">Cycling</h1>
+        <h1 className="absolute left-16 font-bold top-5">Talkshow</h1>
         <img src="interview/logo.svg" className="w-25 h-10 absolute right-5 bottom-4"/>
       </div>
-      
+      {children}
     </div>
     )
   }
   
-  export default Interview
+  export default TalkshowLayout
   
---
+++ components/Talkshow/CardOpen.tsx
@@ -0,0 +1,58 @@
+import { StaticImageData } from "next/image";
+import Image from "next/image";
+import calendar from "../../public/talkshow/calendar.svg";
+import clock from "../../public/talkshow/jam.svg";
+import location from "../../public/talkshow/location.svg";
+export default function CardOpen({
+  title,
+  image,
+  place,
+  time,
+  date,
+  desc,
+  url,
+  isOpen,
+}: {
+  title: string;
+  image: StaticImageData;
+  place: string;
+  time: string;
+  date: string;
+  desc: string;
+  url: string;
+  isOpen: boolean;
+}) {
+  return (
+    <section>
+      <div className="lg:rounded-full flex flex-col gap-10 items-center bg-black text-white p-10 lg:py-5 px-5 mx-10">
+        <div className="flex flex-col lg:flex-row justify-center items-center gap-5">
+          <h1 className={`font-semibold lg:w-[30%] text-5xl italic ${isOpen ? "" : "hidden"}`}>
+            {title}
+          </h1>
+          <Image width={400} height={400} src={image} alt={title} />
+          <div className="grid grid-cols-1">
+            <h1
+              className={`font-bold text-5xl italic ${isOpen ? "hidden" : ""}`}
+            >
+              {title}
+            </h1>
+            <div className={`grid grid-cols-2 mt-5 ${isOpen?'':'w-1/2'}`}>
+              <Image className="mb-2 mx-auto " width={45} height={45} src={calendar} alt="date icon" />
+              <p className="my-auto">{date}</p>
+
+              <Image className="mb-2 mx-auto " width={25} height={25} src={location} alt="loc icon" />
+              <p className="my-auto">{place}</p>
+
+              <Image className="mb-2 mx-auto " width={30} height={30} src={clock} alt="clock icon" />
+              <p className="my-auto">{time}</p>
+            </div>
+          </div>
+        </div>
+        <p className="w-3/4 mx-auto">{desc}</p>
+      </div>
+      <button className={`buttonRed block mx-auto px-10 ${isOpen ? "text-center" : "ml-10"}`}>
+        {isOpen ? "REGISTER NOW!" : "CLOSED"}
+      </button>
+    </section>
+  );
+}
