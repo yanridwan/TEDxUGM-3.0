9fa824019c95602c58b096f12f02153bd89b4adf
nandaisnanda <anandashabrinaputrig@mail.ugm.ac.id>
2023-08-07 16:44:56 +0700

    docs

--- app/documentaries/page.tsx
+++ app/documentaries/page.tsx
@@ -6,7 +6,37 @@ function Documentaries() {
       <h1 className="absolute left-16 font-bold top-5">Documentaries</h1>
       <img src="interview/logo.svg" className="w-25 h-10 absolute right-5 bottom-4"/>
     </div>
-    
+    <div className=" flex flex-row">
+      <h1 className="text-white font-bold text-4xl mt-36 ml-20 -mb-10">The Local Project</h1>
+    </div>
+    <div className="flex justify-center items-center h-screen">
+      <iframe
+        width="560"
+        height="315"
+        src="https://youtube.com/embed/K5H-GvnNz2Y"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+    <div className=" flex flex-row">
+      <h1 className="text-white font-bold text-4xl mt-10 ml-20 -mb-10">A point of View</h1>
+    </div>
+    <div className="flex justify-center items-center h-screen">
+      <iframe
+        width="560"
+        height="315"
+        src="https://youtube.com/embed/K5H-GvnNz2Y"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+    <div class="box-content bg-white w-full h-16 object-bottom">
+      <h1 className="font-bold justify-center items-center text-2xl">histtorycal gap is due to xxxxxxxx</h1>
+    </div>
   </div>
   )
 }
