2755501f995aced5da500c5591a18de32fcde8e0
korbanbanjir <gelora.dam2003@mail.ugm.ac.id>
2023-08-07 21:16:47 +0700

    add(Cycling): readded cycling page & merged main

--- app/cycling/page.tsx
+++ app/cycling/page.tsx
@@ -1,7 +1,7 @@
 function Interview() {
   return (
-    <div class="relative bg-black min-h-screen ">
-    <div class="fixed box-content bg-white w-full h-16">
+    <div className="relative bg-black min-h-screen ">
+    <div className="fixed box-content bg-white w-full h-16">
       <img src="interview/arrow.svg" className="w-5 h-5 absolute left-10 top-5"/>
       <h1 className="absolute left-16 font-bold top-5">Cycling</h1>
       <img src="interview/logo.svg" className="w-25 h-10 absolute right-5 bottom-4"/>
