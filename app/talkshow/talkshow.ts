badcdb281b1f9b1451f132f6df7aa19e719446cf
Khairul <khairulihsannudin@mail.ugm.ac.id>
2023-08-11 17:45:09 +0700

    adding some missing

---
+++ app/talkshow/talkshow.ts
@@ -0,0 +1,29 @@
+import logo from '../../public/logoBlack.png';
+const talkshowData =[
+    {
+        title:'Jimmy Fallon Talkshow',
+        image:logo,
+        place:'UGM',
+        date:'2023-08-18 19:00:00',
+        time:'19.00 - 21.00 WIB',
+        desc:'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet v vLorem ipsum dolor sit amet Lorem ipsum dolor sit amet v vvLorem ipsum dolor sit ametv v Lorem ipsum dolor sit amet',
+    },
+    {
+        title:'Jimmy Talkshow',
+        image:logo,
+        place:'UGM',
+        date:'2023-09-22 19:00:00',
+        time:'TBA',
+        desc:'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet v vLorem ipsum dolor sit amet Lorem ipsum dolor sit amet v vvLorem ipsum dolor sit ametv v Lorem ipsum dolor sit amet',
+    },
+    {
+        title:'Fallon Talkshow',
+        image:logo,
+        place:'UGM',
+        date:'2023-10-28 19:00:00',
+        time:'TBA',
+        desc:'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet v vLorem ipsum dolor sit amet Lorem ipsum dolor sit amet v vvLorem ipsum dolor sit ametv v Lorem ipsum dolor sit amet',
+    },
+]
+
+export default talkshowData;
