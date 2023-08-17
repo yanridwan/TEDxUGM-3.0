95ebaa8e19ad8de34d1db27e2084068a01909d9c
Khairul <khairulihsannudin@mail.ugm.ac.id>
2023-08-17 16:20:17 +0700

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,37 @@
-"use client"
-import './globals.css'
-import { Inter } from 'next/font/google'
+'use client';
+import './globals.css';
+import { Inter } from 'next/font/google';
 import React, { useState } from 'react';
-import { metadata } from './metadata.ts';  
+import { metadata } from './metadata.ts';
 import Navbar from '@/components/Navbar/Navbar';
 import Footer from '@/components/Footer/Footer';
+import link from 'next/link';
+import head from 'next/head';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-    const [openModal, setOpenModal] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
 
-    const handleModalOpen = () => {
-      setOpenModal(true);
-    };
-  
-    return (
+  const handleModalOpen = () => {
+    setOpenModal(true);
+  };
+
+  return (
     <html lang="en">
+      <head>
+        <title>TEDxUGM 2023</title>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body>
-      <div>
-        <Navbar handleModalOpen={handleModalOpen} />
-        {children}
-        <Footer />
-      </div>
+        <div>
+          <Navbar handleModalOpen={handleModalOpen} />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
-  )
+  );
 }
