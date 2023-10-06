562f394f10d7f0b654afffbbe77d9e53b1e1fd93
yanridwan <yanridwan.yr@gmail.com>
2023-10-06 17:34:39 +0700

    comit

--- app/merchandise/[merch]/page.tsx
+++ app/merchandise/[merch]/page.tsx
@@ -8,7 +8,7 @@ export default function Page({ params, searchParams }: { params: { merch: string
             </div>
             <section className="bgMerch flex items-center justify-center h-screen ">
                 <div className="flex flex-col md:flex-row gap-5 md:gap-20 mx-10 rounded-lg  relative z-30 sm:mx-20 aspect-square md:items-center justify-center">
-                <img src={`/${searchParams.image}`} alt={productName} className="w-[30vw] rounded-3xl aspect-square" placeholder="blur"/>
+                    <img src={`/${searchParams.image}`} alt={productName} className="w-[30vw] rounded-3xl border-solid border-4 aspect-square" placeholder="blur" />
                 <div className="flex flex-col gap-1 md:w-[calc(30%+5rem)]">
                     <h1 className="text-black font-bold text-3xl">{productName}</h1>
                     <h1 className="font-bold text-redTED text-xl">{searchParams.price}</h1>
